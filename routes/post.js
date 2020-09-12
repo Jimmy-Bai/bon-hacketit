48f197f611f9aec8e51dc4193cd6764bd461d98f
Jimmy-Bai <jbai0616@gmail.com>
2020-09-12 16:00:16 -0400

    add links

--- routes/post.js
+++ routes/post.js
@@ -6,12 +6,12 @@ const Router = Express.Router();
 module.exports = function(io) {
     // Homepage before login
     Router.get('/restaurant', (req, res) => {
-        res.render('index');
+        res.render('postrestaurant');
     });
 
     // Home page after login
     Router.get('/recipe', (req, res) => {
-        res.render('dashboard');
+        res.render('postrecipe');
     });
 
     return Router;
